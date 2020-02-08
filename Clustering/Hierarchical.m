d863d4e724899daa85cb342775efc797608fc9b5
againstlaw <43742861+againstlaw@users.noreply.github.com>
2020-02-08 01:18:14 -0800

    Add files via upload

---
+++ Clustering/Hierarchical.m
@@ -0,0 +1,76 @@
+clc
+clear all
+
+
+% Load data
+filename = 'countryriskdata.csv';
+Data=readtable(filename);
+
+myData=Data{:,3:6};
+
+% standardization
+% Standardized z-scores
+Z = zscore(myData);
+
+
+% % Hierarchical Clustering
+% eucD = pdist(Z,'euclidean');
+% clustTreeEuc = linkage(eucD,'average');
+% 
+% % The cophenetic correlation is one way to verify that the cluster tree 
+% % is consistent with the original distances. 
+% % Large values indicate that the tree fits the distances well
+% cophenet(clustTreeEuc,eucD)
+% 
+% 
+% % To visualize the hierarchy of clusters
+% [h,nodes] = dendrogram(clustTreeEuc,0);
+% h_gca = gca;
+% h_gca.TickDir = 'out';
+% h_gca.TickLength = [.002 0];
+% h_gca.XTickLabel = [];
+
+
+
+% Hierarchical Clustering
+cosD = pdist(Z,'cosine');
+clustTreeCos = linkage(cosD,'average');
+cophenet(clustTreeCos,cosD)
+
+
+% To visualize the hierarchy of clusters
+figure(1)
+[h,nodes] = dendrogram(clustTreeCos,0);
+h_gca = gca;
+h_gca.TickDir = 'out';
+h_gca.TickLength = [.002 0];
+h_gca.XTickLabel = [];
+
+
+ptsymb = {'bs','r^','md','go','c+'};
+
+hidx = cluster(clustTreeCos,'criterion','distance','cutoff',0.9);
+
+figure(2)
+for i = 1:3
+    clust = find(hidx==i);
+    plot3(Z(clust,2),Z(clust,3),Z(clust,4),ptsymb{i});
+    hold on
+end
+hold off
+
+colnames=Data.Properties.VariableNames;
+names=colnames(3:6);
+
+xlabel(names(2));
+ylabel(names(3));
+zlabel(names(4));
+grid on
+
+
+
+countries=Data.Country;
+countries(hidx==1)   % high risk
+countries(hidx==2)   % moderate risk
+countries(hidx==3)   % low risk
+
